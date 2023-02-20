34fd9db1909b838a5c178f565b1b0f1aef53958b
srd785 <107203103+srd785@users.noreply.github.com>
2023-02-20 23:06:19 +0530

    Update provider.tf

--- aws/ec-2/provider.tf
+++ aws/ec-2/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "eu-west-2"
-  access_key = "xxxxxxAKIA63J"
-  secret_key = "xxxxxvt8a5yzNJyY"
+  access_key = "AKIATG7BYF2NFZIRMGE7"
+  secret_key = "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"
 }
